996745531168be6e1ac4e00a9ee3c1482e44d66e
Arvind Shelke <ucdonor5@gmail.com>
2021-09-25 13:39:21 +0530

    authy, Profile, html added

---
+++ Chatroom/frontend/src/components/auth/Register.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Register() {
+    return (
+        <div className="container pt-5">
+        <div className="card">
+            <div className="card-content">
+                <form className="">
+                <div className="field">
+                <p className="control has-icons-left has-icons-right">
+                    <input className="input" type="email" placeholder="Email"/>
+                    <span className="icon is-small is-left">
+                    <i className="fas fa-envelope"></i>
+                    </span>
+                    <span className="icon is-small is-right">
+                    <i className="fas fa-check"></i>
+                    </span>
+                </p>
+                </div>
+                <div className="field">
+                <p className="control has-icons-left">
+                    <input className="input" type="password" placeholder="Password"/>
+                    <span className="icon is-small is-left">
+                    <i className="fas fa-lock"></i>
+                    </span>
+                </p>
+                </div>
+                <div className="field">
+                <p className="control">
+                    <button className="button is-success">Register</button>
+                </p>
+                </div>
+                </form>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Register
